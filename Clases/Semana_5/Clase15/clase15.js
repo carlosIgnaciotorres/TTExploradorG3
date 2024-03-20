cf637105db80b63c0ae3f57e9a1457f706597b6d
Carlos Torres-Londoño <carlos.torreslo@campusucc.edu.co>
2024-03-20 11:20:54 -0500

    Semana6

---
+++ Clases/Semana_5/Clase15/clase15.js
@@ -0,0 +1,5 @@
+//Palindromo: Realiza un script que determine si la cadena de texto que se le pide al usuario es un palíndromo, es decir, si se lee de la misma forma desde la izquierda y desde la derecha
+
+function Palindromo(cadena ) {
+    arreglo = 
+}
