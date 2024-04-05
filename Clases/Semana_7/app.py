5c790be4f5b5cc11d1a822190a0be3b7bee1506e
Carlos Ignacio Torres L <carlonchotorres@gmail.com>
2024-04-05 13:15:42 -0500

    flask

--- Clases/Semana_7/app.py
+++ Clases/Semana_7/app.py
@@ -3,8 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    nombre = input("Cual es tu nombre?")
+    return 'hola '+ nombre
+
+@app.route('/login')
+def login():
+    return("Aquí estara el login")
+
+@app.route('/boyaca')
+def boyaca():
+    return "Aquí viene Boyaca"
+
+@app.route('/boyaca/aves')
+def avesBoy():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
- 
